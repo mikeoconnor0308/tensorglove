046caaccf3ee51980960ed20940cfb70baafa47c
Mike O'Connor <mikeoconnor0308@gmail.com>
2018-04-27 14:54:23 +0100

    refactor and add unity sample project

---
+++ glovetest/Assets/Recording/Hi5CsvRecorder.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using HI5;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Recording
+{
+	public class Hi5CsvRecorder : MonoBehaviour
+	{
+
+		private enum GestureType { None, Fist, Click, Point}
+		
+		private CsvFileWriter csvLeftWriter;
+		private CsvFileWriter csvRightWriter;
+
+		[SerializeField]
+		private HI5_TransformInstance leftHand;
+		[SerializeField]
+		private HI5_TransformInstance rightHand;
+
+		[SerializeField] private GestureType gesture; 
+		
+		private bool recordingRightHand;
+		private bool recordingLeftHand;
+
+		[SerializeField]
+		private Text recordingLeftText; 
+		[SerializeField]
+		private Text recordingRightText;
+
+		[SerializeField]
+		private bool recordPositions = false;
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				if (recordingLeftHand)
+				{
+					csvLeftWriter.Dispose();
+					recordingLeftHand = false;
+					recordingLeftText.text = "Left Hand: Not Recording";
+				}
+				else
+				{
+					InitialiseWriter(true, gesture);
+					recordingLeftHand = true;
+					recordingLeftText.text = "Left Hand: Recording " + gesture.ToString();
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				if (recordingRightHand)
+				{
+					csvRightWriter.Dispose();
+					recordingRightHand = false;
+					recordingRightText.text = "Right Hand: Not Recording";
+				}
+				else
+				{
+					InitialiseWriter(false, gesture);
+					recordingRightHand = true;
+					recordingRightText.text = "Right Hand: Recording " + gesture.ToString();
+				}
+			}
+
+			if (recordingLeftHand)
+			{
+				WriteDataFrame(csvLeftWriter, leftHand, gesture);
+			}
+
+			if (recordingRightHand)
+			{
+				WriteDataFrame(csvRightWriter, rightHand, gesture);
+			}
+		}
+
+		private void InitialiseWriter(bool left, GestureType gesture)
+		{
+			if (left)
+			{
+				
+				csvLeftWriter = new CsvFileWriter("C:\\Users\\Mike.DESKTOP-CA70LTI\\Code\\iSci\\research\\nsb\\glovetest\\TrainingData\\" + "left_" + gesture + "_" +  System.DateTime.Now.ToString("yyyyMMddHHmmss"));
+				Debug.Log("Writing to path: " + Application.dataPath);
+				WriteHeader(csvLeftWriter, leftHand);
+			}
+			else
+			{
+				csvRightWriter = new CsvFileWriter("C:\\Users\\Mike.DESKTOP-CA70LTI\\Code\\iSci\\research\\nsb\\glovetest\\TrainingData\\" + "right_" + gesture + "_" +  System.DateTime.Now.ToString("yyyyMMddHHmmss"));
+				Debug.Log("Writing to path: " + Application.dataPath);
+				WriteHeader(csvRightWriter, rightHand);
+			}
+			
+		}
+
+		private void WriteHeader(CsvFileWriter writer, HI5_TransformInstance hand)
+		{
+			List<string> header = new List<string>();
+			string[] coords = {"X", "Y", "Z", "W"};
+			foreach (var t in hand.HandBones)
+			{
+				if (recordPositions)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						header.Add(string.Format("{0}_Pos_{1}", t.name, coords[i]));
+					}
+				}
+				for (int i = 0; i < 4; i++)
+				{
+					header.Add(string.Format("{0}_Quat_{1}", t.name, coords[i]));
+				}
+			}
+			header.Add("Gesture");
+			writer.WriteRow(header);
+			writer.Flush();
+		}
+
+
+		private void WriteDataFrame(CsvFileWriter writer, HI5_TransformInstance hand, GestureType gesture)
+		{
+			List<string> frame = new List<string>();
+			foreach (var t in hand.HandBones)
+			{
+				if (recordPositions)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						frame.Add(t.localPosition[i].ToString());
+					}
+				}
+				for (int i = 0; i < 4; i++)
+				{
+					frame.Add(t.localRotation[i].ToString());
+				}
+			}
+			frame.Add(gesture.ToString());
+			writer.WriteRow(frame);
+			writer.Flush();
+		}
+	}
+}
---
+++ glovetest/Assets/TensorflowOscGesture.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Net;
+using HI5;
+using Rug.Osc;
+using UnityEngine;
+
+/// <summary>
+/// Class for communicating with the tensorglove Osc server.
+/// </summary>
+public class TensorflowOscGesture : MonoBehaviour
+{
+    //ip address the tensorglove server is using.
+    [SerializeField] private string ipAddress = "127.0.0.1";
+
+    // port to transmit to server.
+    [SerializeField] private int sendPort = 54321;
+
+    // port to receive from server.
+    [SerializeField] private int recvPort = 54322;
+
+    private OscListener listener;
+
+    private OscSender sender;
+
+    // how long to wait before transmitting positions to tensorflow server.
+    [Range(0f, 10f)] [SerializeField] private float waitTime = 5f;
+
+    // the hand to use (only works with right hand at the moment).
+    [SerializeField] private HI5_TransformInstance hand;
+
+    // glove material, for color.
+    [SerializeField] private Material gloveMaterial;
+
+    // colors to assign to each gesture.
+    [SerializeField] private Color[] gestureColors;
+
+    private bool runningGestures = false;
+
+    [SerializeField] private GameObject laser;
+
+    private int prediction = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        listener = new OscListener(IPAddress.Parse(ipAddress), recvPort);
+        listener.Connect();
+        listener.Attach("/prediction", OnPrediction);
+
+
+        sender = new OscSender(IPAddress.Parse(ipAddress), sendPort);
+        sender.Connect();
+    }
+
+    private void OnPrediction(OscMessage message)
+    {
+        Debug.Log("Prediction Received " + message[0]);
+        prediction = (int) message[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (runningGestures == false)
+        {
+            runningGestures = true;
+            StartCoroutine(GestureDetection());
+        }
+
+        gloveMaterial.color = gestureColors[prediction];
+        // pointing class.
+        if (prediction == 3)
+        {
+            laser.gameObject.SetActive(true);
+        }
+        else
+        {
+            laser.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator GestureDetection()
+    {
+        while (runningGestures)
+        {
+            SendCurrentPositions();
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private void SendCurrentPositions()
+    {
+        // send the quaternions of each finger. 
+        string[] coords = {"X", "Y", "Z", "W"};
+        object[] featureValues = new object[hand.HandBones.Length * 4];
+        int boneIndex = 0;
+        foreach (var t in hand.HandBones)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                featureValues[boneIndex * 4 + i] = (t.localRotation[i]);
+            }
+
+            boneIndex++;
+        }
+
+        //send the quaternion.
+        OscMessage message = new OscMessage("/predict", featureValues);
+        sender.Send(message);
+    }
+}
