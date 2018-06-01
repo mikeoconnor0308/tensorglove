639daf61ea7415d702dbba1edeff3765162e847d
Alex Binnie <alexjbinnie@gmail.com>
2018-06-01 17:00:01 +0100

    Added characters that walk around

---
+++ glovetest/Assets/Character/Character.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Character : MonoBehaviour {
+
+    bool IsRagdoll = false;
+
+    public GameObject RagdollRoot;
+
+    public static List<Character> Characters = new List<Character>();
+
+    public List<Renderer> BodyRenderers = new List<Renderer>();
+
+	// Use this for initialization
+	void Awake () {
+        SetRagdoll(false);
+        var color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        foreach(var renderer in BodyRenderers) {
+            renderer.material.color = color;
+        }
+        Characters.Add(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetRagdoll(bool active) {
+        foreach(var collider in this.transform.GetComponentsInChildren<Collider>()) {
+            if (collider.transform == this.transform)
+                collider.enabled = !active;
+            else
+                collider.enabled = active;
+        }
+        foreach (var rigidbody in this.transform.GetComponentsInChildren<Rigidbody>())
+        {
+            if (rigidbody.transform == this.transform)
+                rigidbody.isKinematic = active;
+            else
+                rigidbody.isKinematic = !active;
+        }
+        if(active) {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<ThirdPersonCharacter>().enabled = false;
+            GetComponent<NavMeshAgent>().enabled = false;
+        }
+        if(active) {
+            Characters.Remove(this);
+        }
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.gameObject.name != "Ground" && !IsRagdoll && collision.rigidbody != null)
+        {
+            if(collision.rigidbody.velocity.magnitude > 4f || collision.transform.gameObject.name == "Fist")
+                SetRagdoll(true);
+        }
+	}
+}
---
+++ glovetest/Assets/Character/RandomWalk.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class RandomWalk : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+    public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+    public Vector3? target = null;  // target to aim for
+
+    public Bounds Area;
+
+    private void Start()
+    {
+        // get the components on the object we need ( should not be null due to require component so no need to check )
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+    }
+
+
+    private void Update()
+    {
+        if(!target.HasValue || Random.value < Time.deltaTime * 0.2f)
+            SetTarget(new Vector3(Random.Range(Area.min.x, Area.max.x),
+                                  Random.Range(Area.min.y, Area.max.y),
+                                  Random.Range(Area.min.z, Area.max.z)));
+
+        if (agent.enabled)
+        {
+            agent.SetDestination(target.Value);
+
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+                SetTarget(null);
+            }
+        }
+    }
+
+
+    public void SetTarget(Vector3? target)
+    {
+        this.target = target;
+    }
+
+	private void OnDrawGizmosSelected()
+	{
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Area.center, Area.size);
+	}
+}
